ef84790a9e474296a3db71694c771f1fba4d6938
Anglfdz <20223tn026@utez.edu.mx>
2024-11-27 13:45:06 -0600

    [UPDATE] add viwe edit for employees, and adapter switch in distints cases

--- FrontEnd/src/Catalogue-client/pages/ListItemsPage.jsx
+++ FrontEnd/src/Catalogue-client/pages/ListItemsPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import ItemsList from '../../employee/components/ItemsList';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
 function ListItemsPage() {
-  return (
-    <div>ListItemsPage</div>
-  )
+    return (
+        <>
+            <div className="grid w-full bg-bluegray-200 p-3">
+                <div className="p-inputgroup flex-1 pr-2">
+                    <span className="p-inputgroup-addon">
+                        <i className="pi pi-search"></i>
+                    </span>
+                    <InputText placeholder="Buscar producto" />
+                </div>
+                <Button label="Añadir productos" className=" p-button-rounded p-button-text bg-blue-600 text-50" />
+            </div>
+            <ItemsList page='employee/items' />
+        </>
+    )
 }
 
 export default ListItemsPage
